c078648639e4a44565156b2cd80c6c95dba926c5
ethanmac <ethanmac@mac.com>
2021-01-23 14:05:41 +0800

    update

--- exam/exam_ronin_tcn_train.py
+++ exam/exam_ronin_tcn_train.py
@@ -237,7 +237,7 @@ class RonninLstmTrain(object):
         return get_model(args)
 
     @classmethod
-    def inspect_model(cls, model, batch_input_size=(2, 400, 6)):
+    def inspect_model(cls, model, batch_input_size):  # (2, 400, 6)
         from source.ronin_lstm_tcn import inspect_model
         inspect_model(model, batch_input_size=batch_input_size)
 
